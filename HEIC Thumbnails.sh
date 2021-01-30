0ba12026e7ca4e8b8e1815d590cf3d820c82175a
PewDieMelon <74740271+PewDieMelon@users.noreply.github.com>
2021-01-30 09:57:36 +0100

    Add files via upload

---
+++ HEIC Thumbnails.sh
@@ -0,0 +1,49 @@
+###################################
+# compile the codecs and software #
+###################################
+
+sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+sudo apt update
+sudo apt install -y build-essential autoconf libtool git-core build-dep imagemagick libmagickcore-dev libde265 libheif
+cd /usr/src/ 
+sudo git clone https://github.com/strukturag/libde265.git  
+sudo git clone https://github.com/strukturag/libheif.git 
+cd libde265/ 
+sudo ./autogen.sh 
+sudo ./configure 
+sudo make  
+sudo make install 
+cd /usr/src/libheif/ 
+sudo ./autogen.sh 
+sudo ./configure 
+sudo make  
+sudo make install 
+cd /usr/src/ 
+sudo wget https://www.imagemagick.org/download/ImageMagick.tar.gz 
+sudo tar xf ImageMagick.tar.gz 
+cd ImageMagick-7*
+sudo ./configure --with-heic=yes 
+sudo make  
+sudo make install  
+sudo ldconfig
+
+#######################
+# activate PHP plugin #
+#######################
+
+sudo phpize
+sudo phpenmod imagick
+sudo systemctl restart apache2
+
+###################################
+# check if it is set up correctly #
+###################################
+
+sudo php -r 'phpinfo();' | grep HEIC
+
+#################################################
+# if HEIC is highlighted in the output, you are #
+# good to go                                    #
+#################################################
+
+sudo reboot
